3854361b7d042d21d65bc5723fa162b4a0e5c5c6
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-07-01 22:21:23 +0700

    homework 5 gmail integration

--- src/main/java/com/ttsr/springshop/frontend/CartView.java
+++ src/main/java/com/ttsr/springshop/frontend/CartView.java
@@ -1,12 +1,13 @@
 package com.ttsr.springshop.frontend;
 
 import com.ttsr.springshop.model.Cart;
-import com.ttsr.springshop.model.Product;
+import com.ttsr.springshop.model.Order;
+import com.ttsr.springshop.model.repository.OrderRepository;
 import com.ttsr.springshop.service.CartService;
+import com.ttsr.springshop.service.MailService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
@@ -14,7 +15,7 @@ import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Route("cart")
 public class CartView extends VerticalLayout {
@@ -22,8 +23,14 @@ public class CartView extends VerticalLayout {
 
     private final CartService cartService;
 
-    public CartView(CartService cartService) {
+    private final OrderRepository orderRepository;
+
+    private final MailService mailService;
+
+    public CartView(CartService cartService, OrderRepository orderRepository, MailService mailService) {
         this.cartService = cartService;
+        this.orderRepository = orderRepository;
+        this.mailService = mailService;
 
         initCartGrid();
         add(grid,initMainButton());
@@ -60,5 +67,15 @@ public class CartView extends VerticalLayout {
 
             return new HorizontalLayout(plusButton, minusButton);
         }));
+
+        var button = new Button("Checkout", e -> {
+            var order = new Order();
+            order.setId(UUID.randomUUID());
+            orderRepository.save(order);
+
+            mailService.sendMessage("i.a.polovnikov@gmail.com","Your order successful has been processed");
+        });
+
+        add(grid,button);
     }
 }
