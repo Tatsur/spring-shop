f031c67fb66cd95b6685f1289eb79677ead865d9
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-07-01 22:27:31 +0700

    homework 4 revert

--- src/main/java/com/ttsr/springshop/frontend/CartView.java
+++ src/main/java/com/ttsr/springshop/frontend/CartView.java
@@ -4,7 +4,6 @@ import com.ttsr.springshop.model.Cart;
 import com.ttsr.springshop.model.Order;
 import com.ttsr.springshop.model.repository.OrderRepository;
 import com.ttsr.springshop.service.CartService;
-import com.ttsr.springshop.service.MailService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -25,12 +24,9 @@ public class CartView extends VerticalLayout {
 
     private final OrderRepository orderRepository;
 
-    private final MailService mailService;
-
-    public CartView(CartService cartService, OrderRepository orderRepository, MailService mailService) {
+    public CartView(CartService cartService, OrderRepository orderRepository) {
         this.cartService = cartService;
         this.orderRepository = orderRepository;
-        this.mailService = mailService;
 
         initCartGrid();
         add(grid,initMainButton());
@@ -67,15 +63,5 @@ public class CartView extends VerticalLayout {
 
             return new HorizontalLayout(plusButton, minusButton);
         }));
-
-        var button = new Button("Checkout", e -> {
-            var order = new Order();
-            order.setId(UUID.randomUUID());
-            orderRepository.save(order);
-
-            mailService.sendMessage("i.a.polovnikov@gmail.com","Your order successful has been processed");
-        });
-
-        add(grid,button);
     }
 }
